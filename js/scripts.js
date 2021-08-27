2edd1e6c6954db2232b45482503efae4701db933
Henrique Albuquerque <henriquealbuquerquef@gmail.com>
2021-08-27 07:30:09 -0300

    finalização

--- js/scripts.js
+++ js/scripts.js
@@ -20,11 +20,31 @@ for (let i = 0; i < boxes.length; i++) {
             this.appendChild(cloneElemento);
             if (player1 == player2) { //computando a jogada
                 player1++;
+
+                if (secondPlayer == "ia-jogador") {
+                    computerPlay();
+                    player2++;
+                }
             } else {
                 player2++;
             }
             CheckWin(); //check vitoria
         }
+    });
+}
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function () {
+        secondPlayer = this.getAttribute("id");
+
+        for (y = 0; y < buttons.length; y++) {
+            buttons[y].style.display = 'none';
+        }
+
+        setTimeout(function () {
+            let container = document.querySelector('#container');
+            container.classList.remove('hide')
+        }, 250);
     });
 }
 
@@ -38,15 +58,15 @@ function checkElemento(player1, player2) { //ver quem vai jogar
 }
 
 function CheckWin() {
-    let bloco1 = document.querySelector("block-1");
-    let bloco2 = document.querySelector("block-2");
-    let bloco3 = document.querySelector("block-3");
-    let bloco4 = document.querySelector("block-4");
-    let bloco5 = document.querySelector("block-5");
-    let bloco6 = document.querySelector("block-6");
-    let bloco7 = document.querySelector("block-7");
-    let bloco8 = document.querySelector("block-8");
-    let bloco9 = document.querySelector("block-9");
+    let bloco1 = document.getElementById("block-1");
+    let bloco2 = document.getElementById("block-2");
+    let bloco3 = document.getElementById("block-3");
+    let bloco4 = document.getElementById("block-4");
+    let bloco5 = document.getElementById("block-5");
+    let bloco6 = document.getElementById("block-6");
+    let bloco7 = document.getElementById("block-7");
+    let bloco8 = document.getElementById("block-8");
+    let bloco9 = document.getElementById("block-9");
 
     if (bloco1.childNodes.length > 0 && bloco2.childNodes.length > 0 && bloco3.childNodes.length) {
         let bloco1Child = bloco1.childNodes[0].className;
@@ -55,10 +75,10 @@ function CheckWin() {
 
         if (bloco1Child == 'x' && bloco2Child == 'x' && bloco3Child == 'x') {
             //x venceu
-            alert('X venceu!');
+            declareWinner('x');
         } else if (bloco1Child == 'o' && bloco2Child == 'o' && bloco3Child == 'o') {
             //o venceu
-            alert('Y venceu!')
+            declareWinner('o');
         }
     }
 
@@ -69,10 +89,10 @@ function CheckWin() {
 
         if (bloco4Child == 'x' && bloco5Child == 'x' && bloco6Child == 'x') {
             //x venceu
-            alert('X venceu!');
+            declareWinner('x');
         } else if (bloco4Child == 'o' && bloco5Child == 'o' && bloco6Child == 'o') {
             //o venceu
-            alert('Y venceu!')
+            declareWinner('o');
         }
     }
     if (bloco7.childNodes.length > 0 && bloco8.childNodes.length > 0 && bloco9.childNodes.length) {
@@ -82,10 +102,10 @@ function CheckWin() {
 
         if (bloco7Child == 'x' && bloco8Child == 'x' && bloco9Child == 'x') {
             //x venceu
-            alert('X venceu!');
+            declareWinner('x');
         } else if (bloco7Child == 'o' && bloco8Child == 'o' && bloco9Child == 'o') {
             //o venceu
-            alert('Y venceu!')
+            declareWinner('o');
         }
     }
     if (bloco1.childNodes.length > 0 && bloco4.childNodes.length > 0 && bloco7.childNodes.length) {
@@ -95,11 +115,126 @@ function CheckWin() {
 
         if (bloco1Child == 'x' && bloco4Child == 'x' && bloco7Child == 'x') {
             //x venceu
-            alert('X venceu!');
+            declareWinner('x');
         } else if (bloco1Child == 'o' && bloco4Child == 'o' && bloco7Child == 'o') {
             //o venceu
-            alert('Y venceu!')
+            declareWinner('o');
+        }
+    }
+    if (bloco2.childNodes.length > 0 && bloco5.childNodes.length > 0 && bloco8.childNodes.length) {
+        let bloco2Child = bloco2.childNodes[0].className;
+        let bloco5Child = bloco5.childNodes[0].className;
+        let bloco8Child = bloco8.childNodes[0].className;
+
+        if (bloco2Child == 'x' && bloco5Child == 'x' && bloco8Child == 'x') {
+            //x venceu
+            declareWinner('x');
+        } else if (bloco2Child == 'o' && bloco5Child == 'o' && bloco8Child == 'o') {
+            //o venceu
+            declareWinner('o');
+        }
+    }
+    if (bloco3.childNodes.length > 0 && bloco6.childNodes.length > 0 && bloco9.childNodes.length) {
+        let bloco3Child = bloco3.childNodes[0].className;
+        let bloco6Child = bloco6.childNodes[0].className;
+        let bloco9Child = bloco9.childNodes[0].className;
+
+        if (bloco3Child == 'x' && bloco6Child == 'x' && bloco9Child == 'x') {
+            //x venceu
+            declareWinner('x');
+        } else if (bloco3Child == 'o' && bloco6Child == 'o' && bloco9Child == 'o') {
+            //o venceu
+            declareWinner('o');
+        }
+    }
+    if (bloco1.childNodes.length > 0 && bloco5.childNodes.length > 0 && bloco9.childNodes.length) {
+        let bloco1Child = bloco1.childNodes[0].className;
+        let bloco5Child = bloco5.childNodes[0].className;
+        let bloco9Child = bloco9.childNodes[0].className;
+
+        if (bloco1Child == 'x' && bloco5Child == 'x' && bloco9Child == 'x') {
+            //x venceu
+            declareWinner('x');
+        } else if (bloco1Child == 'o' && bloco5Child == 'o' && bloco9Child == 'o') {
+            //o venceu
+            declareWinner('o');
+        }
+    }
+    if (bloco3.childNodes.length > 0 && bloco5.childNodes.length > 0 && bloco7.childNodes.length) {
+        let bloco3Child = bloco3.childNodes[0].className;
+        let bloco5Child = bloco5.childNodes[0].className;
+        let bloco7Child = bloco7.childNodes[0].className;
+
+        if (bloco3Child == 'x' && bloco5Child == 'x' && bloco7Child == 'x') {
+            //x venceu
+            declareWinner('x');
+        } else if (bloco3Child == 'o' && bloco5Child == 'o' && bloco7Child == 'o') {
+            //o venceu
+            declareWinner('o');
         }
     }
 
-};
+    let counter = 0;
+    for (let i = 0; i < boxes.length; i++) {
+        if (boxes[i].childNodes[0] != undefined) {
+            counter++;
+        }
+    }
+    if (counter == 9) {
+        declareWinner('Ninguém venceu!');
+    }
+
+    function declareWinner(winner) {
+        let scoreboardX = document.querySelector("#scoreboard-1");
+        let scoreboardO = document.querySelector("#scoreboard-2");
+        let msg = '';
+
+        if (winner == 'x') {
+            scoreboardX.textContent = parseInt(scoreboardX.textContent) + 1;
+            msg = 'O jogador 1 venceu!';
+        } else if (winner == 'o') {
+            scoreboardO.textContent = parseInt(scoreboardO.textContent) + 1;
+            msg = 'O jogador 2 venceu!'
+        } else {
+            msg = 'Ninguém venceu!';
+        }
+
+        setTimeout(function () {
+            messageText.innerHTML = msg;
+            messageContainer.classList.remove("hide");
+        }, 250)
+        setTimeout(function () {
+            messageContainer.classList.add("hide");
+            let boxesRemove = document.querySelectorAll(".box div");
+            for (let i = 0; boxesRemove.length; i++) {
+                boxesRemove[i].parentNode.removeChild(boxesRemove[i]);
+            }
+        }, 2000);
+
+        player1 = 0;
+        player2 = 0;
+    }
+}
+
+function computerPlay() {
+    let cloneO = o.cloneNode(true);
+    counter = 0;
+    filled = 0;
+    
+    for (let i = 0; i < boxes.length; i++) {
+        let randomN = Math.floor(Math.random() * 5);
+        if (boxes[i].childNodes[0] == undefined) {
+            if (randomN <= 1) {
+                boxes[i].appendChild(cloneO);
+                counter++;
+                break;
+            }
+        } else {
+            filled++;
+        }
+    }
+
+    if (counter == 0 && filled < 9) {
+        computerPlay();
+    }
+}
